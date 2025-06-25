d087c15a8e60e1fb1c13f68f399c5f13dca5932e
AnanthaKannan <sreeananthakannan@gmail.com>
2025-06-25 21:55:07 +0530

    unit test basic

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SimplePost } from "./components/1Simple/SimplePost";
+function App() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <SimplePost />
+    </>
+  );
+}
+
+export default App;
